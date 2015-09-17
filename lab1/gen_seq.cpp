10425d94b28d7b99a0bdd4fd66e3842caf9c2ffc
Igor Schelkonogov <ingver.yum@gmail.com>
2015-09-17 22:18:46 +0500

    Added time test

--- lab1/gen_seq.cpp
+++ lab1/gen_seq.cpp
@@ -5,6 +5,9 @@
 #include <cstring>
 #include <algorithm>
 #include <cmath>
+#include <ctime>
+#include <climits>
+#include <cfloat>
 
 #define M_PI 3.14159265358979323846
 
@@ -38,19 +41,61 @@ int_seq iseq_arr[7] = { ord, rord, saw, sinusoid,
 double_seq dseq_arr[7] = { ord, rord, saw, sinusoid, 
 	stepped, quazi_ord, rand_seq };
 
-#define SIZE 113
-
 int main()
 {
-	int iarr[SIZE] = { 0 };
-	double darr[SIZE] = { 0 };
-	srand( time( 0 ) );
-	
-	if( stepped( darr, SIZE, 3, 203, 11 ) )
-		show_arr( darr, SIZE );
-	else
-		cout << "error\n";
-	
+	int    * iarr  = NULL;
+	double * darr  = NULL;
+	int imin       = 0;
+	int imax       = INT_MAX;
+	double dmin    = 0;
+	double dmax    = DBL_MAX;
+	clock_t time1  = 0;
+	clock_t time2  = 0;
+	clock_t total1 = 0;
+	clock_t total2 = 0;
+	int size       = 0;
+	cout << "***************************\n";
+	cout << "Testing functions for INTS:\n";
+	total1 = clock();
+	for( int i = 0; i < 7; ++i ) {
+		for( int j = 0; j < 10; ++j ) {
+			size = (j+1) * 5 * 10e5;
+			iarr = new int[size];
+			time1 = clock();
+			iseq_arr[i]( iarr, size, imin, imax, 100 );
+			time2 = clock();
+			delete[] iarr;
+
+			cout << "Envoked " << fn_str[i] << "() for " 
+			     << size << " of ints; time passed: "
+			     << 1000.0*(time2-time1)/CLOCKS_PER_SEC
+			     << "ms \n";
+			
+		}
+		cout << endl;
+	}
+	cout << endl;
+	cout << "******************************\n";
+	cout << "Testing functions for DOUBLES:\n";
+	for( int i = 0; i < 7; ++i ) {
+		for( int j = 0; j < 10; ++j ) {
+			size = (j+1) * 5 * 10e5;
+			darr = new double[size];
+			time1 = clock();
+			dseq_arr[i]( darr, size, dmin, dmax, 100 );
+			time2 = clock();
+			delete[] darr;
+
+			cout << "Envoked " << fn_str[i] << "() for " 
+			     << size << " of doubles; time passed: "
+			     << 1000.0*(time2-time1)/CLOCKS_PER_SEC
+			     << "ms \n";
+		}
+		cout << endl;
+	}
+	total2 = clock();
+	cout << "Total time: " 
+	     << (total2-total1)/CLOCKS_PER_SEC << "s\n";
 	return 0;
 }
 
