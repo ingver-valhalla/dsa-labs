b7615228d9a3224ef91439c7021da1a8771be368
Igor Schelkonogov <ingver.yum@gmail.com>
2015-11-20 10:44:32 +0500

    Updated test for key

--- lab3/test_key.cpp
+++ lab3/test_key.cpp
@@ -8,20 +8,46 @@ using namespace std;
 
 int main()
 {
+	cout << "==============================" << endl;
+	cout << "LAUNCHING TEST" << endl;
+	cout << "==============================" << endl;
 	assert( Key().val() == 0 );
 	assert( Key(1).val() == 1 );
+
 	assert( Key(3) == Key(3) );
+	assert(! (Key(2) == Key(3)) );
+
 	assert( Key(4) != Key(5) );
+	assert(! (Key(3) != Key(3)) );
+
 	assert( Key(5) < Key(6) );
+	assert(! (Key(5) < Key(5)) );
+	assert(! (Key(5) < Key(4)) );
+
 	assert( Key(7) > Key(6) );
-	assert( Key::comparisons() == 4 );
+	assert(! (Key(7) > Key(7)) );
+	assert(! (Key(7) > Key(8)) );
+
+	assert( Key(10) >= Key(10) );
+	assert( Key(10) >= Key(9) );
+	assert(! (Key(10) >= Key(11)) );
+
+	assert( Key(10) <= Key(10) );
+	assert( Key(10) <= Key(11) );
+	assert(! (Key(10) <= Key(9)) );
+
+	assert( Key::comparisons() == 20 );
 	Key::reset_comps();
 	assert( Key::comparisons() == 0 );
+
 	Key a;
 	a = 10;
 	assert( Key::assignments() == 1 );
 	Key::reset_assigns();
 	assert( Key::assignments() == 0 );
 
+	cout << "==============================" << endl;
+	std::cout << "TEST PASSED" << std::endl;
+	cout << "==============================" << endl;
 	return 0;
 }
