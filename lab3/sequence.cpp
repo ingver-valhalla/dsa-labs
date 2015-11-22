92d6e658d03f2a95e344d3ed80958d9da2c36b8e
Igor Schelkonogov <ingver.yum@gmail.com>
2015-11-22 18:29:19 +0500

    Added odd and sparse sequences

--- lab3/sequence.cpp
+++ lab3/sequence.cpp
@@ -1,6 +1,7 @@
 // sequence.cpp
 
 #include <cstdlib>
+#include <climits>
 
 #include "sequence.h"
 
@@ -10,6 +11,7 @@ int make_ord( Key * arr, int size, int min, int max )
 		return 0;
 	for( int i = 0; i < size; ++i ) {
 		arr[i] = (int)(min + i * (double)(max - min) / (size - 1) + 0.5);
+		
 	}
 	return 1;
 }
@@ -20,5 +22,29 @@ int make_rand( Key * arr, int size, int min, int max )
 		return 0;
 	for( int i = 0; i < size; ++i ) 
 		arr[i] = (int)(rand() % (max-min+1) + min);
+	return 1;
+}
+
+int make_odd( Key * arr, int size, int min, int max )
+{
+	if( !arr || size <= 0 )
+		return 0;
+	for( int i = 0; i < size; ++i ) 
+		arr[i] = i * 2 + 1;
+	return 1;
+}
+
+int make_sparse( Key * arr, int size, int min, int max )
+{
+	if( !arr || size <= 0 )
+		return 0;
+
+	int step = INT_MAX/ size;
+
+	arr[0] = 0;
+	for( int i = 0; i < size - 1; ++i ) 
+		arr[i] = arr[i-1].val() + step;
+	arr[size-1] = INT_MAX;
+
 	return 1;
 }
--- lab3/sequence.h
+++ lab3/sequence.h
@@ -4,3 +4,8 @@
 
 int make_ord( Key * arr, int size, int min, int max );
 int make_rand( Key * arr, int size, int min, int max );
+int make_odd( Key * arr, int size, int min = 0, int max = 0 );
+int make_sparse( Key * arr, int size, int min = 0, int max = 0 );
+
+
+typedef int (*Make_seq_p)( Key*, int, int, int );
