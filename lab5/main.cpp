fffc9b0d91831e74a60876462433ba7debd44b5f
Igor Schelkonogov <ingver.yum@gmail.com>
2016-05-02 22:17:58 +0500

    lab5: file output

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include "tree.h"
 
+FILE* fi = NULL;
+
 int get_rand()
 {
 	return rand() % 1001 - 500;
@@ -31,6 +33,7 @@ void test_rand_bst( pIns insert )
 	pNode tr = NULL;
 
 	printf( "Inserting random keys...\n\n" );
+	fprintf( fi, "random keys...\n\n" );
 
 	for( int num = INIT; num <= FINAL; num += STEP ) {
 
@@ -47,10 +50,12 @@ void test_rand_bst( pIns insert )
 		}
 		printf( "elements: %d, avg. tree height: %lf\n", num, 
 		        (double)height_sum / TESTS );
+		fprintf( fi, "(%d;%.2lf)\n", num, (double)height_sum / TESTS );
 	}
 
 	printf( "\n\n" );
 	printf( "Inserting ordered keys...\n\n" );
+	fprintf( fi, "\nordered keys...\n\n" );
 
 	for( int num = INIT; num <= FINAL; num += STEP ) {
 
@@ -67,12 +72,20 @@ void test_rand_bst( pIns insert )
 		}
 		printf( "elements: %d, avg. tree height: %lf\n", num, 
 		        (double)height_sum / TESTS );
+		fprintf( fi, "(%d;%.2lf)\n", num, (double)height_sum / TESTS );
 	}
 }
 
 int main()
 {
 	srand( time( NULL ) );
+
+	fi = fopen( "out.txt", "w" );
+	if( fi == NULL ) {
+		fprintf( stderr, "Failed to open file\n" );
+		exit( 1 );
+	}
+
 	pNode tr = NULL;
 	for( int i = 0; i < 10; ++i ) {
 		int num = get_rand();
@@ -82,25 +95,31 @@ int main()
 		//getchar();
 	}
 	PrintTree( tr );
+	printf( "CountSum:\n" );
+	printf( "\nresult: %d\n", CountSum( tr, print ));
 
 	printf( "tree height: %d\n", TreeHeight( tr ) );
 	printf( "nodes count: %d\n", CountNodes( tr ) );
 
-	/*printf( "VisitPost: " );
-	VisitPost( tr, mul2 );
-	printf( "\n" );*/
+	//printf( "VisitPost: " );
+	//VisitPost( tr, mul2 );
+	//printf( "\n" );
 
-	//printf( "CountSum: %d\n", CountSum( tr, print ) );
-
-	//printf( "SumOfInner: %d\n", SumOfInner( tr ) );
+	printf( "SumOfInner: %d\n", SumOfInner( tr ) );
 	
+	printf( "\n*******************\n" );
 	printf( "Testing BST\n" );
+	fprintf( fi, "Testing BST\n" );
 	test_rand_bst( Insert );
 	printf( "\n*******************\n" );
 	printf( "Testing Randomized BST\n" );
+	fprintf( fi, "\nTesting Randomized BST\n" );
 	test_rand_bst( InsertRand );
 	
 	//getchar();
+	FreeTree( &tr );
+
+	fclose( fi );
 
 	return 0;
 }
